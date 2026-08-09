a80004af4d34ca05db014a76e9d3af87d754b953
Amritansh singh <amritansh.25gcebec121@galgotiacollege.edu>
2026-08-09 23:34:45 +0530

    feat: integrate Tailwind CSS and implement color picker functionality
    - Added Tailwind CSS as a dependency and configured it in vite.config.js
    - Updated App component to include a color picker input and display the selected color
    - Styled the App component using Tailwind CSS classes
    - Imported Tailwind CSS in the index.css file

--- color-picker/src/App.jsx
+++ color-picker/src/App.jsx
@@ -5,11 +5,17 @@ import heroImg from './assets/hero.png'
 
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [color, setColor] = useState('#ffffff')
+  
   return (
     <>
-     
+    <h1 className='text-blue-400'>Color picker</h1>
+     <div className='flex flex-col justify-center items-center'>
+      <div style={{backgroundColor:color,height:100 ,width:100}}></div>
+      <p>Selected color {color}</p>
+      <label>Select a color: </label>
+        <input type='color' onChange={(e)=>setColor(event.target.value)}/>
+     </div>
     </>
   )
 }
