0a313a61413d17cbfa8340587a59b3bfdc5eb86f
Amritansh singh <amritansh.25gcebec121@galgotiacollege.edu>
2026-08-10 12:32:04 +0530

    feat: add SVG assets for React and Vite, create Header component, and set up Tailwind CSS with Vite

---
+++ react-router/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className='bg-red-500 p-4 m-2'>Hello this is a react router project</h1>
+    </>
+  )
+}
+
+export default App
