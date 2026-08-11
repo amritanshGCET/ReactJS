6beab89d0f443a95c9cd79d6096a00a43937c2ed
Amritansh singh <amritansh.25gcebec121@galgotiacollege.edu>
2026-08-11 13:12:39 +0530

    feat: restructure App component and add Header, Footer, and Home components

--- react-router/src/App.jsx
+++ react-router/src/App.jsx
@@ -2,14 +2,18 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
-
+import Header from './components/Header/header'
+import Footer from './components/Footer/footer'
+import Home from './components/home/home'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <h1 className='bg-red-500 p-4 m-2'>Hello this is a react router project</h1>
+     <Header />
+     <Footer />
+     <Home />
     </>
   )
 }
