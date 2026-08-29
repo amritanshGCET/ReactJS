49fecab8aff7d0a03d77e9cf1b48a0c7f61c9024
Amritansh singh <amritansh.25gcebec121@galgotiacollege.edu>
2026-08-30 01:50:07 +0530

    created a new project with context-Api

---
+++ context-project/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { ThemeProvider } from './contexts/theme'
+function App() {
+  const [themeMode,setThemeMode]=useState("light")
+  const lightTheme=()=>{
+    setThemeMode("light")
+  }
+  const darkTheme=()=>{
+    setThemeMode("dark")
+  }
+  return (
+  <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        {/* theme-btn */}
+                    </div>
+                    {/*card*/}
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+</ThemeProvider>
+  )
+}
+
+export default App
