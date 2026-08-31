09659915945e0fe54b7f756bb35991920c6798ec
Amritansh singh <amritansh.25gcebec121@galgotiacollege.edu>
2026-09-01 01:00:21 +0530

    added dark mode light mode toggle in context project

--- context-project/src/App.jsx
+++ context-project/src/App.jsx
@@ -1,5 +1,7 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import { ThemeProvider } from './contexts/theme'
+import ThemeBtn from './components/button'
+import Card from './components/card'
 function App() {
   const [themeMode,setThemeMode]=useState("light")
   const lightTheme=()=>{
@@ -8,14 +10,20 @@ function App() {
   const darkTheme=()=>{
     setThemeMode("dark")
   }
+
+  useEffect(()=>{
+    document.querySelector('html').classList.remove("light","dark")
+    document.querySelector('html').classList.add(themeMode)
+  },[themeMode])
+
   return (
   <ThemeProvider value={{themeMode,lightTheme,darkTheme}}>
 <div className="flex flex-wrap min-h-screen items-center">
                 <div className="w-full">
                     <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
-                        {/* theme-btn */}
+                     <ThemeBtn/>
                     </div>
-                    {/*card*/}
+                    <Card/>
                     <div className="w-full max-w-sm mx-auto">
                        
                     </div>
